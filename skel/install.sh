88c2ab9232e898dfbe8b766bc0ea4f84b09a27f6
Ryo Nakamura <nakamura@ins.tl.fukuoka-u.ac.jp>
2023-04-30 12:53:31 +0900

    add installation of emacs

--- skel/install.sh
+++ skel/install.sh
@@ -2,7 +2,7 @@
 
 # xpywm installation script for Debian GNU/Linux.
 
-sudo apt install -y xbase-clients rxvt-unicode xfonts-terminus net-tools redshift python3-pip python3-venv
+sudo apt install -y xbase-clients rxvt-unicode xfonts-terminus net-tools redshift python3-pip python3-venv emacs
 python3 -m venv ~/.venv
 source ~/.venv/bin/activate
 pip3 install xpywm xpymon xpylog
