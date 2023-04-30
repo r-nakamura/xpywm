8f2b020a07ec9538ea3424e0145c597d7a917c43
Ryo Nakamura <nakamura@ins.tl.fukuoka-u.ac.jp>
2023-04-30 12:30:51 +0900

    use venv

--- skel/install.sh
+++ skel/install.sh
@@ -2,5 +2,7 @@
 
 # xpywm installation script for Debian GNU/Linux.
 
-sudo apt install -y xbase-clients rxvt-unicode xfonts-terminus net-tools redshift python3-pip
-sudo pip3 install xpywm xpymon xpylog
+sudo apt install -y xbase-clients rxvt-unicode xfonts-terminus net-tools redshift python3-pip python3-venv
+python3 -m venv ~/.venv
+source ~/.venv/bin/activate
+pip3 install xpywm xpymon xpylog
